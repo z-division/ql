599918d8385f35402a8355cb89a63e250462afb1
Patrick <pattyshack101@gmail.com>
2019-06-25 18:50:00 -0700

    remove fake declaration (#37)
    * remove fake declaration
    
    * .

--- parser/declaration.go
+++ parser/declaration.go
@@ -9,18 +9,6 @@ type declaration struct{}
 
 func (declaration) isDeclaration() {}
 
-// TODO(patrick): Remove once we have defined other real declarations
-type FakeDeclaration struct {
-	Location
-	declaration
-
-	Expression Expr
-}
-
-func (fake *FakeDeclaration) String() string {
-	return prettyFormatNode("", fake, 0)
-}
-
 type TypeDef struct {
 	Location
 	declaration
